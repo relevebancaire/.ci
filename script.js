a5ce9f82089e21e720b452fbeb0faa287594d55f
relevebancaire <releve.bancaire.ci@gmail.com>
2025-04-12 21:08:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// Smooth scroll to a section
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
 
-// Contact form submission
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent form submission
-    const name = document.getElementById('name').value;
-    alert(`Thank you, ${name}! Your message has been sent.`);
-});
