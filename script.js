c981e0567525db24809c94cee517f229307b4416
relevebancaire <releve.bancaire.ci@gmail.com>
2025-04-16 18:59:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const releves = [
     { nom: "Relevé bancaire standard", image: "images/societeg.jpg", prix: "10€" },
     { nom: "Relevé bancaire détaillé", image: "images/detaille.png", prix: "20€" },
-    { nom: "Relevé bancaire premium", image: "images/premium.png", prix: "30€" }
+    { nom: "Relevé bancaire premium", image: "images/premium.png", prix: "30€" },
+    { nom: "Relevé bancaire VIP", image: "images/vip.png", prix: "50€" },
+    { nom: "Relevé bancaire entreprise", image: "images/entreprise.png", prix: "40€" },
+    { nom: "Relevé bancaire privé", image: "images/prive.png", prix: "35€" },
+    { nom: "Relevé bancaire express", image: "images/express.png", prix: "15€" },
+    { nom: "Relevé bancaire luxe", image: "images/luxe.png", prix: "60€" }
 ];
 
 function afficherReleves() {
@@ -14,7 +19,7 @@ function afficherReleves() {
 
         div.innerHTML = `
             <h2>${releve.nom}</h2>
-            <img src="${releve.image}" alt="Exemple ${releve.nom}" width="100%">
+            <img src="${releve.image}" alt="${releve.nom}">
             <p>Prix: <span class="price">${releve.prix}</span></p>
         `;
         container.appendChild(div);
